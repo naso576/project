8398928ec372433482840d1800c0f052996b78a5
naso576 <70210241+naso576@users.noreply.github.com>
2024-03-01 21:22:31 +0530

    Delete config directory

--- config/db.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    // url: "mongodb://127.0.0.1:27017/projectnktd"
-    url : "mongodb+srv://nktdc:nktdc1234@nktdc-db.mpo2h2e.mongodb.net/?retryWrites=true&w=majority&appName=nktdc-db"
-  };
-
-
-
-  
