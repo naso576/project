49d6639ed4fc535834bf0d1adbc066748ad27cac
naso576 <70210241+naso576@users.noreply.github.com>
2024-03-02 00:10:34 +0530

    Update index.jsx

--- src/scenes/templates/index.jsx
+++ src/scenes/templates/index.jsx
@@ -443,7 +443,11 @@ console.log('params : '+ JSON.stringify((param1)) );
 
 axios.post('https://server-u74y.vercel.app/createTemplate',(param1) ).then(res => 
 {
-
+ res.setHeader("Access-Control-Allow-Origin", "*")
+res.setHeader("Access-Control-Allow-Credentials", "true");
+res.setHeader("Access-Control-Max-Age", "1800");
+res.setHeader("Access-Control-Allow-Headers", "content-type");
+res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
 }
 ).catch(err => {
 
