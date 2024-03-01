4d4628b9cad2950191069654918781a7a25a7ffd
naso576 <70210241+naso576@users.noreply.github.com>
2024-03-01 21:22:55 +0530

    Delete routes directory

--- routes/tablets.routes.js
+++
@@ -1,8 +0,0 @@
-const tablets =require("../controllers/tablets.controller.js");
-var router = require("express").Router();
-
-
-router.get("/findTabletsList", tablets.findTabletsList);
-router.get("/addTablet", tablets.addTablet);
-
-module.exports = router;    
