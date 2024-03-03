c405d1f989ccfc2421a5b7464b422c975058f9fd
naso576 <70210241+naso576@users.noreply.github.com>
2024-03-03 16:04:52 +0530

    Update index.jsx

--- src/scenes/templates/index.jsx
+++ src/scenes/templates/index.jsx
@@ -442,6 +442,10 @@ const cnt = data.length;
        
        'Access-Control-Allow-Origin': '*',
       'Content-Type': 'application/json',
+         "Access-Control-Allow-Methods" : "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE",
+         "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Credentials" : true,
+        "Access-Control-Max-Age": 1800
      
     };
 
